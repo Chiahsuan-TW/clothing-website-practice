8a66c15c72362ec296a5a0959344f80179b27198
Chiahsuan Chou <belinda05021992@gmail.com>
2022-07-26 09:55:46 +0800

    finish landing-page's categories section

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
+import './categories.styles.scss';
+import categories from './data/categories.json';
 
 const App = () => {
-  const categories = [
-    {id: 1, title: 'Hats'},
-    {id: 2, title: 'Jackets'},
-    {id: 3, title: 'Sneakers'},
-    {id: 4, title: 'Womens'},
-    {id: 5, title: 'Mens'},
-  ]
 
   return  <div className="categories-container">
-      {categories.map(({title, id}) => {
+      {categories.map(({title, id, imageUrl}) => {
       return (<div className="category-container" key={id}>
-        <div className="background-image" />
+        <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}/>
         <div className="category-body-container">
           <h2>{title}</h2>
           <p>Shop Now</p>
