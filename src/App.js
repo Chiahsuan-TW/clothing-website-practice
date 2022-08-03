6b31fdc1c8a0731c38c0eb2ac77b5044d68bb192
Chiahsuan Chou <belinda05021992@gmail.com>
2022-08-03 11:10:11 +0800

    rename component as authentication

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Home from '../src/routes/home/home.component';
 import Navbar from '../src/routes/navigation/navigation.component';
-import SignIn from '../src/routes/signIn/signIn.component';
+import Authentication from './routes/authentication/authentication.component';
 import {Routes, Route} from 'react-router-dom';
 
 
@@ -14,7 +14,7 @@ const App = () => {
     <Route path='/' element={<Navbar/>} >
       <Route index element={<Home/>} />
       <Route path='/shop' element={<Shop/>} />
-      <Route path='/sign-in' element={<SignIn/>} />
+      <Route path='/auth' element={<Authentication/>} />
     </Route>
   </Routes>)
 }
