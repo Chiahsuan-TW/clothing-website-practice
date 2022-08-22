7fb15ae3b22042752366460cef39a47513a1fd4e
Chiahsuan Chou <belinda05021992@gmail.com>
2022-08-22 11:05:02 +0800

    feat: navigate to specific page when clicking the category block on home page

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,10 +1,13 @@
 import {CategoryContainer, Background, Body} from './category-item.styles.jsx';
-
+import {useNavigate} from 'react-router-dom';
 
 const CategoryItem = ({category}) => {
-  const {title, imageUrl} = category;
+  const {title, imageUrl, route} = category;
   
-  return (<CategoryContainer>
+  const navigate = useNavigate();
+  const handleNavigate = () => navigate(route);
+
+  return (<CategoryContainer onClick={handleNavigate}>
     <Background imageUrl={imageUrl}/>
     <Body>
       <h2>{title}</h2>
--- src/components/menu/menu.component.jsx
+++ src/components/menu/menu.component.jsx
@@ -1,8 +1,8 @@
 import CategoryItem from '../category-item/category-item.component';
 import { CategoriesContainer } from './menu.styles';
 
-
 const Menu = ({categories}) => {
+  
   return  (<CategoriesContainer>
       {categories.map((category) => (<CategoryItem key={category.id} category={category} />))}
     </CategoriesContainer>)
